cb84cb3013ec6c3d0417042bc64988ec31fc60de
yamamoto91 <94538002+yamamoto91@users.noreply.github.com>
2022-01-03 19:45:01 +0900

    locationを実装

--- script.js
+++ script.js
@@ -30,3 +30,16 @@ function refresh() {
 }
 
 recalc();
+
+//ロケーション
+const lang = document.querySelector('html').lang;
+
+if(lang === 'ja') {
+  document.querySelector('option[value="index.html"]').selected = true;
+} else if(lang === 'en') {
+  document.querySelector('option[value="index-en.html"]').selected = true;
+}
+
+document.getElementById('form').select.onchange = function() {
+  location.href = document.getElementById('form').select.value;
+}
